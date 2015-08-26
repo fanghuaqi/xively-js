b6e8e647e34b323e113fd3b4a1f7594f34adf0eb
Huaqi Fang <huaqi.fang@synopsys.com>
2015-08-26 14:33:36 +0800

    Changes to embARC SmartHOME test

--- sample-app/js/script.js
+++ sample-app/js/script.js
@@ -1,11 +1,11 @@
 (function ( $ ){
   "use strict";
 
-  var feedID = 79650;
+  var feedID = 1125809548;
 
   // SET API KEY
   
-  xively.setKey( "FzZOVUxKRPl_Tvvtr1z77b8gOVCSAKxlTnlwSzQrZ3U4MD0g" ); // do not use this one, create your own at xively.com
+  xively.setKey( "1utiYwVwm7QH1KCQowljICDEvikbpbCEUs0mDktH4baqg4Va" ); // do not use this one, create your own at xively.com
 
   // get all feed data in one shot
 
@@ -65,41 +65,17 @@
       datastream = feed.datastreams[x];
       value = parseInt(datastream["current_value"]);
 
-      // LIGHTS
+      // Room Light 1
 
-      if ( datastream.id === "lights" ) {
-        handleToggle( "lights", value );
+      if ( datastream.id === "bedroom_light" ) {
+        handleToggle( "bedroom_light", value );
       }
 
-      // TV
+      // Room Light 1
 
-      if ( datastream.id === "tv-state" ) {
-        handleToggle( "tv-state", value );
+      if ( datastream.id === "parlor_light" ) {
+        handleToggle( "parlor_light", value );
       }
-
-      // MUSIC
-
-      if ( datastream.id === "volume" ) {
-        var $range = $(".js-volume");
-        
-        // set value
-        $range.val(value);
-
-        // save changes
-        $range.on("custom-change", function( event, val ) {
-          $(".app-state").addClass("loading").fadeIn(200);
-          xively.datastream.update(feedID, "volume", { "current_value": val }, function(){
-            $(".app-state").removeClass("loading").fadeOut(200);
-          });
-        });
-
-        // make it live
-        xively.datastream.subscribe(feedID, "volume", function ( event, data ) {
-          ui.fakeLoad();
-          $range.val(parseInt(data["current_value"]));
-        });
-
-      }      
 
       // TEMPERATURE
 
